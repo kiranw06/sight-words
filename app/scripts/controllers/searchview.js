7437ecb666e215418d5d4cd04ff098bcf39781be
Kiranw06 <kiranw6691@gmail.com>
2016-11-29 12:58:34 -0800

    started local storage function

--- app/scripts/controllers/searchview.js
+++ app/scripts/controllers/searchview.js
@@ -8,10 +8,60 @@
  * Controller of the sightWordsApp
  */
 angular.module('sightWordsApp')
-  .controller('SearchviewCtrl', function ($scope, search) {
+  .controller('SearchviewCtrl', function ($scope, search, $localStorage) {
  	 $scope.refreshsearch = function(term){
     	$scope.suggestedWords = search.query({
     		s: term 
     	});
     };
-  });
+
+
+    //Define Local Storage
+    $scope.storage = $localStorage;
+    //TODO Error?
+    $scope.listName = "";
+
+
+    //Stores List Data
+
+	$scope.saveList = function(text){
+	   var listData = {
+	       'name': text,
+	       // TODO how to input words?
+	       'words': [],
+	   };
+
+
+        var save = true;
+
+        if (!$scope.storage.savedPatches) {
+            $scope.storage.savedPatches = [];
+            
+        }else {
+            for(var i = 0; i < $scope.storage.savedPatches.length; i++){
+                if (listData.patchName === $scope.storage.savedPatches[i].patchName){
+                    save = false; 
+                }
+            }
+        } 
+        
+
+
+        if (save===true){
+            listData.patchId = $scope.storage.savedPatches.length + 1;
+            $scope.storage.savedPatches.push(listData);
+            $scope.patchSaved = {
+                'success': true
+            };
+        } else {
+            console.log('patch already saved');
+             $scope.patchSaved = {
+                'duplicate': true
+            };
+     	}
+
+	};
+
+
+
+});
