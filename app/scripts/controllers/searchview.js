7aafbea4fb1c05119276198038c1bd607c131db4
Kiranw06 <kiranw6691@gmail.com>
2016-12-01 11:58:18 -0800

    display selected words JSON

--- app/scripts/controllers/searchview.js
+++ app/scripts/controllers/searchview.js
@@ -16,10 +16,16 @@ angular.module('sightWordsApp')
     };
 
 
-    //Define Local Storage
+//Define Global Variables
     $scope.savedLists = $localStorage.savedLists;
+    $scope.displayText = "";
+    // object that stores an array of words selected by user
+    $scope.selectedWords = [];
+    // displays selected words to user
+    $scope.selectedWord = $scope.savedLists.listData;
 
     
+
     // Check if saved lists exists
     if ($scope.savedLists) {
     	console.log('loaded saved word lists');
@@ -31,42 +37,37 @@ angular.module('sightWordsApp')
     }
 
 
-   // Makes slug for list name
-   function slugify(text){
-     return text.toString().toLowerCase()
-       .replace(/\s+/g, '')           // Replace spaces with nothing
-       .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-       .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-       .replace(/^-+/, '')             // Trim - from start of text
-       .replace(/-+$/, '');            // Trim - from end of text
-   }
-   
+    // Makes slug for list name type
+    function slugify(text){
+      return text.toString().toLowerCase()
+        .replace(/\s+/g, '')           // Replace spaces with nothing
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
+    }
+
     
     //Stores List Data
-	$scope.saveList = function(text){
-	   
-		var slug = slugify(text);
-	   	var listData = {
-	   	    'name': text,
-	   	    // TODO how to input words?
-	   	    'words': $scope.selectedWords,
-	   	    'slug': slug 
-	   	};
-	   // Add data to local storage	
-	   $localStorage.savedLists[slug] = listData;  
+    $scope.saveList = function(text){
+       
+    	var slug = slugify(text);
 
-	};
+       	var listData = {
+       	    'name': text,
+            'slug': slug,
+       	    // words type stored in an array
+       	    'words': $scope.selectedWords 
+       	};
 
+       // Add data to local storage	
+       $localStorage.savedLists[slug] = listData;     
+    };
 
-	//TODO Error?
-    $scope.displayText = "";
-
-    $scope.selectedWords = [];
-
+    
+    // Select/Deselect words
     $scope.selectWord = function(word){
-
     	$scope.selectedWords.push(word);
-
     };
 
 
