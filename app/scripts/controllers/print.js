00eb94c5e3e9f1271264e9f98b60ab714e55c02c
Kiranw06 <kiranw6691@gmail.com>
2016-12-11 23:28:36 -0800

    inject print stylesheet via javascript in print.js

--- app/scripts/controllers/print.js
+++ app/scripts/controllers/print.js
@@ -42,7 +42,8 @@ angular.module('sightWordsApp')
         var printContents = document.getElementById("printable").innerHTML;
         var popupWin = window.open('Print Me', '_blank');
         popupWin.document.open();
-        popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="../styles/main.css"></head><body onload="window.print()">' + printContents + '</body></html>');
+        // added styles for print stylesheet as style element
+        popupWin.document.write('<html><body onload="window.print()"><style>@page{size:A4;}ul{list-style-type:none;}div.printable{width:100%;height:100%;}div.printable-card{width:5in;height:3in;margin:10px;border-radius:30px;border-style:dashed;border-width:thick;background-color:rgb(255, 232, 164);page-break-inside:avoid;}div.card-word{font-size:xx-large;font-family:sans-serif;text-align:center;margin-top:16%;margin-bottom:20%;}</style>' + printContents + '</body></html>');
         popupWin.document.close();
       }; 
 
